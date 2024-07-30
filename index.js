75c23517b3055fc1998c9357ef891aba948678ab
leproduction <108595211+leproduction@users.noreply.github.com>
2024-07-30 15:41:08 -0400

    Update index.js

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ app.post('/signup', async (req, res) => {
             return res.status(400).json({ message: "User already exists" });
         }
         
-        const hashedPassword = await bcrypt.hash(password, 10);
+        const hashedPassword = bcrypt.hash(password, 10);
         const newUser = new RegisterModel({ name, email, tel, password: hashedPassword });
         await newUser.save();
         console.log("New user created:", newUser);
